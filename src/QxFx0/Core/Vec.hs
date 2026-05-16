060876dfecc6afb1bce368f72a20b312b142925a
QxFx0 <glebgnilitski@gmail.com>
2026-05-17 02:04:19 +0300

    refactor(cabal): remove unused shim facade modules

--- src/QxFx0/Core/Vec.hs
+++
@@ -1,4 +0,0 @@
-{-| Core-level re-export of vector primitives from `QxFx0.Types.Vec`. -}
-module QxFx0.Core.Vec (module QxFx0.Types.Vec) where
-
-import QxFx0.Types.Vec
--- src/QxFx0/Runtime/Context.hs
+++
@@ -1,5 +0,0 @@
-module QxFx0.Runtime.Context
-  ( module QxFx0.Runtime.Wiring
-  ) where
-
-import QxFx0.Runtime.Wiring
